6af8c4c209f19a7c2b05ff7c4fc03e9e358c669b
Arun Bose <arun@core.ai>
2022-02-16 17:04:14 +0530

    post files working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express()
 const multer  = require('multer')
-const upload = multer({ dest: 'www/' })
+const fs = require('fs')
 const port = 3000
 
 app.use('/', express.static('www'))
+
+var multipartUpload = multer({storage: multer.diskStorage({
+        destination: function (req, file, callback) {
+            let path = './www/'+req.body['path'] || './www';
+            if (!fs.existsSync(path)) {
+                fs.mkdirSync(path);
+            }
+            callback(null, path);
+            },
+        filename: function (req, file, callback) {
+            callback(null, file.originalname);
+        }})
+}).array('files');
+
+app.post('/upload', multipartUpload, function (req, res, next) {
+    // req.files is array of `photos` files
+    // req.body will contain the text fields, if there were any
+    res.send('ok')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
