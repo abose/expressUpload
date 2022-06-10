db4f641d6dca31280a2c01d355f557c5697e5cd3
Arun Bose <arun@core.ai>
2022-06-10 21:31:49 +0530

    getGitHubZip api

--- src/index.js
+++ src/index.js
@@ -8,9 +8,22 @@ import cors from 'cors'
 import { mkdtemp, rm , mkdir} from 'fs/promises';
 import scrape from 'website-scraper';
 import zipper from 'zip-local';
+import fetch from 'node-fetch';
+import crypto from'crypto';
+
+const downloadFile = (async (url, path) => {
+    const res = await fetch(url);
+    const fileStream = fs.createWriteStream(path);
+    await new Promise((resolve, reject) => {
+        res.body.pipe(fileStream);
+        res.body.on("error", reject);
+        fileStream.on("finish", resolve);
+    });
+});
 
 const port = 3000;
 const TEMP_DIR = "./temp";
+const TEMP_GITHUB_DIR = "./temp/GitHub";
 
 app.use(cors({
     methods: ['POST', 'GET', 'OPTIONS']
@@ -51,6 +64,15 @@ app.post('/upload', multipartUpload, function (req, res, next) {
     res.send('ok')
 })
 
+async function _ensureTempDirs() {
+    if(!fs.existsSync(TEMP_DIR)){
+        await mkdir(TEMP_DIR, {recursive: true});
+    }
+    if(!fs.existsSync(TEMP_GITHUB_DIR)){
+        await mkdir(TEMP_GITHUB_DIR, {recursive: true});
+    }
+}
+
 async function _silentDeleteDir(uploadPath) {
     try {
         await rm(uploadPath,{recursive: true});
@@ -74,9 +96,7 @@ app.get('/getWebsiteCode', async function (req, res, next) {
 
         console.log(`${clientIP}: Downloading Site: ${downloadURL}`);
         let siteName = new URL(downloadURL).hostname;
-        if(!fs.existsSync(TEMP_DIR)){
-            await mkdir(TEMP_DIR, {recursive: true});
-        }
+        await _ensureTempDirs();
         tempDir = path.join(TEMP_DIR, `${siteName}-`);
         tempDir = await mkdtemp(tempDir);
 
@@ -98,6 +118,44 @@ app.get('/getWebsiteCode', async function (req, res, next) {
     } catch (err) {
         console.error(err);
         await _silentDeleteDir(tempDir);
+        res.status(500);
+        res.send("something went wrong.");
+        return;
+    }
+})
+
+app.get('/getGitHubZip', async function (req, res, next) {
+    try {
+        let downloadOrg = req.query.org;
+        let downloadRepo = req.query.repo;
+        let clientIP= req.headers["x-real-ip"] || req.headers['X-Forwarded-For'] || req.socket.remoteAddress;
+        console.log(`${clientIP}: Downloading github: ${downloadOrg}/${downloadRepo}`);
+        if(!downloadOrg || !downloadRepo){
+            res.status(400);
+            res.send("missing `org` or `repo` parameter");
+            return;
+        }
+        await _ensureTempDirs();
+        let zipURL = `https://api.github.com/repos/${downloadOrg}/${downloadRepo}/zipball`;
+        let tempFilePath = `${TEMP_DIR}/GitHub/${downloadOrg}-${downloadRepo}-${crypto.randomBytes(4).readUInt32LE(0)}.zip`;
+        await downloadFile(zipURL, tempFilePath);
+
+        // GiHub does not put content size during zip file download, so we check size post download
+        var stats = fs.statSync(tempFilePath)
+        var fileSizeInBytes = stats.size;
+        var fileSizeInMegabytes = fileSizeInBytes / (1024*1024);
+        if(fileSizeInMegabytes > 25){
+            res.status(413);
+            res.send("File size too large. Phoenix services only support 25MB file size for non logged in users");
+            return;
+        }
+
+        let readStream = fs.createReadStream(tempFilePath);
+        readStream.pipe(res);
+
+        await _silentDeleteDir(tempFilePath);
+    } catch (err) {
+        console.error(err);
         res.status(500);
         res.send("something went wrong.");
         return;
